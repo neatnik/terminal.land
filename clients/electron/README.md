8aea32ffab2a9d8bc51be57767f383afc3261842
Adam Newbold <adam@neatnik.net>
2020-10-03 23:18:57 -0400

    Added Electron client

---
+++ clients/electron/README.md
@@ -0,0 +1,3 @@
+# Electron Client
+
+The [Electron](https://www.electronjs.org) client can run on most common operating systems, and features Discord integration (which is pretty cool!). Beyond the Discord integration and the convenience of a dedicated app, there isn’t much difference between this client and the [web version](https://terminal.land).
---
+++ clients/electron/src/index.js
@@ -0,0 +1,93 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const rpc = require('discord-rich-presence')('756623176397750272');
+const path = require('path');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+	app.quit();
+}
+
+const windowStateKeeper = require('electron-window-state');
+const createWindow = () => {
+  
+  let mainWindowState = windowStateKeeper({
+    defaultWidth: 1000,
+    defaultHeight: 800
+  });
+    
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    //width: 800,
+    //height: 600,
+    x: mainWindowState.x,
+    y: mainWindowState.y,
+    width: mainWindowState.width,
+    height: mainWindowState.height,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    //titleBarStyle: "hidden",
+    frame: false
+  });
+  
+  mainWindowState.manage(mainWindow);
+  
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  
+  // Maximize the window
+  //mainWindow.maximize();
+  
+  // Open the DevTools
+  //mainWindow.webContents.openDevTools();
+  
+  // Handle refreshes
+  mainWindow.webContents.on('did-fail-load', () => {
+  	mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  });
+  
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+app.on('quit', () => {
+	// Quit.
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
+
+ipcMain.on('status', function(e, value) {
+	setPresence(value.place, 'Connected as ' + value.person);
+});
+
+function setPresence(details, state) {
+	rpc.updatePresence({
+		details: details,       
+		state: state,
+		largeImageKey: 'tl-icon-master',
+		largeImageText: 'Terminal Land'
+	})
+}
+
+setPresence('Nowhere Land', 'Disconnected');
