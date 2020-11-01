a28658773fe097b178b1fae48f06b3451449ac1d
Adam Newbold <adam@neatnik.net>
2020-11-01 15:27:26 -0500

    v0.0.1

--- clients/electron/src/index.js
+++ clients/electron/src/index.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
+const { autoUpdater } = require("electron-updater");
 const rpc = require('discord-rich-presence')('756623176397750272');
 const path = require('path');
 
@@ -52,7 +53,11 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+//app.on('ready', createWindow);
+app.on('ready', function()  {
+	createWindow();
+	autoUpdater.checkForUpdatesAndNotify();
+});	
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
